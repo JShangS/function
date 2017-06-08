46d2a125291024b1f6da4cb18753b87cd6389727
JShangS <424431687@qq.com>
2017-06-08 10:20:14 +0800

    加了些画图显示的注释

--- 粒子群/JS_PSO.m
+++ 粒子群/JS_PSO.m
@@ -4,11 +4,11 @@ close all
 %%%基本粒子群算法，根据《Particle Swarm Optimization》 James Kennedy
 %%By Jerry Shang,JS
 %%2017/6/5
-N = 30;                                    %粒子群规模，即粒子数
+N = 10;                                    %粒子群规模，即粒子数
 D = 2;                                     %粒子的维数，即寻优参数的个数
-X_pso = 1-2*rand(D,N);                         %初始化粒子群的位置
+X_pso = 10-20*rand(D,N);                     %初始化粒子群的位置
 V = rand(D,N);                             %初始化粒子群的速度
-Pbest = 10000*ones(1,N);                         %粒子群的个体最优解
+Pbest = 10000*ones(1,N);                   %粒子群的个体最优解
 Gbest = 0;                                 %粒子群的全体最优解索引
 Max_iter = 500 ;                           %最大迭代次数
 Xmax = 8;                                 %X的最大取值范围[-1,1]
@@ -42,7 +42,9 @@ time1 = toc;
 Gbest = find(Pbest==min(Pbest)); 
 
 disp(['PSO的最佳值: ',num2str(Pbest(:,Gbest))])
+disp(['收敛的坐标:','(',num2str(X_iter(1,end)),',',num2str(X_iter(2,end)),')']);
 disp(['PSO的耗时: ',num2str(time1)])
+
 
 %每一代最优值
 figure()
@@ -73,6 +75,7 @@ L = -Xmax:step:Xmax;
 tic
 Z = f1(X1,X2);
 minZ = min(min(Z));
+[hang,lie] = find(Z==minZ);
 time2 = toc;
 if step >=0.005
    figure()
@@ -80,8 +83,12 @@ if step >=0.005
    contour(X1,X2,Z)
    title('函数图像')
    hold on 
-   plot(X_iter(1,:),X_iter(2,:),'r-*');
+   plot(X_iter(1,:),X_iter(2,:),'b-*');
+   plot(X_iter(1,end),X_iter(2,end),'r*','LineWidth',2,'MarkerSize',12);
+   plot(L(hang),L(lie),'ko','LineWidth',2,'MarkerSize',12)
+   legend('函数值等高线','PSO搜索路径','PSO最终值位置','穷举法的最好值位置')
 %    plot3(X_iter(1,:),X_iter(2,:),Gvalue_iter,'r-*');
 end
 disp(['步长为',num2str(step),'下穷举的最佳值: ',num2str(minZ)])
+disp(['最佳值的坐标:','(',num2str(L(hang)),' , ',num2str(L(lie)),')']);
 disp(['穷举情况下的耗时: ',num2str(time2)])
